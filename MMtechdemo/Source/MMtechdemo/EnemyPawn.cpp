bef6c490c896c056d9dd91fbdff084175e88259e
jeffreybardon <jbardonjr@charter.net>
2015-04-17 15:03:52 -0400

    no message

---
+++ MMtechdemo/Source/MMtechdemo/EnemyPawn.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MMtechdemo.h"
+#include "EnemyPawn.h"
+#include "Steering.h"
+
+
+// Sets default values
+AEnemyPawn::AEnemyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEnemyPawn::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AEnemyPawn::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
